516641969b039d3c5c5c574998cc4db975c03418
ThisZhu <1941956605@qq.com>
2017-04-07 00:17:47 +0800

    changes log 6

---
+++ app/src/main/java/com/zhudi/irestaurant/mine/i.java
@@ -0,0 +1,7 @@
+package com.zhudi.irestaurant.mine;
+
+/**
+ * Created by zhudi on 2017/4/6.
+ */
+public class i {
+}
---
+++ app/src/main/java/com/zhudi/irestaurant/mine/view/CircleImageView.java
@@ -0,0 +1,106 @@
+package com.zhudi.irestaurant.mine.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by zhudi on 2017/4/6.
+ */
+public class CircleImageView extends ImageView {
+    private Paint paint ;
+    private int mBorderWidth = 20;
+    private int mBorderColor = Color.RED;
+    public CircleImageView(Context context) {
+        this(context,null);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        paint = new Paint();
+
+    }
+
+    /**
+     * 绘制圆形图片
+     * @author caizhiming
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Drawable drawable = getDrawable();
+        if (null != drawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            Bitmap b = getCircleBitmap(bitmap, 14);
+            final Rect rectSrc = new Rect(0, 0, b.getWidth(), b.getHeight());
+            final Rect rectDest = new Rect(0,0,getWidth(),getHeight());
+            paint.reset();
+            canvas.drawBitmap(b, rectSrc, rectDest, paint);
+            drawBorder(canvas, bitmap.getWidth(), bitmap.getHeight());
+        } else {
+            super.onDraw(canvas);
+        }
+    }
+
+    /**
+     * 获取圆形图片方法
+     * @param bitmap
+     * @param pixels
+     * @return Bitmap
+     * @author caizhiming
+     */
+    private Bitmap getCircleBitmap(Bitmap bitmap, int pixels) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        int x = bitmap.getWidth();
+        int y = bitmap.getHeight();
+        canvas.drawCircle(x / 2, y/2 , x/ 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+
+
+    }
+    /**
+     * 绘制最外面的边框
+     *
+     * @param canvas
+     * @param width
+     * @param height
+     */
+    private void drawBorder(Canvas canvas, final int width, final int height) {
+        if (mBorderWidth == 0) {
+            return;
+        }
+        final Paint mBorderPaint = new Paint();
+        mBorderPaint.setStyle(Paint.Style.STROKE);
+        mBorderPaint.setAntiAlias(true);
+        mBorderPaint.setColor(mBorderColor);
+        mBorderPaint.setStrokeWidth(mBorderWidth);
+        /**
+         * 坐标x：view宽度的一般 坐标y：view高度的一般 半径r：因为是view的宽度-border的一半
+         */
+        canvas.drawCircle(width, height, width, mBorderPaint);
+    }
+}
